eb0780d47f23baa6613dc299b836ae3efc707838
Paul Coffield <paulcoffield@gmail.com>
2024-02-23 22:47:07 +0000

    Middleware

--- routes/users.js
+++ routes/users.js
@@ -34,16 +34,23 @@ router.post("/", (req, res) => {
 router
   .route("/:id")
   .get((req, res) => {
-    req.params.id;
+    // console.log(req.user);
     res.send(`Get user with ID: ${req.params.id}`);
   })
   .put((req, res) => {
-    req.params.id;
     res.send(`Update user with ID: ${req.params.id}`);
   })
   .delete((req, res) => {
-    req.params.id;
     res.send(`Delete user with ID: ${req.params.id}`);
   });
+
+// Use param to set the user based on the id provided
+// this is then used in the next step (above)
+
+const users = [{ name: "Kyle" }, { name: "Sally" }];
+router.param("id", (req, res, next, id) => {
+  req.user = users[id];
+  next();
+});
 
 module.exports = router;
